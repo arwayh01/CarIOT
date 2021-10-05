b8dc331fd6fe125c9de74ba62bb664fd291fb726
arwayh01 <arwa.yahyaoui@enis.tn>
2021-10-05 23:57:49 +0200

    Initial commit by arwa

---
+++ app/src/main/java/com/example/cariot/Accelerometer.java
@@ -0,0 +1,14 @@
+package com.example.cariot;
+
+public class Accelerometer {
+    public double x;
+    public double y;
+    public double z;
+
+
+    public Accelerometer(double x, double y, double z){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+}
